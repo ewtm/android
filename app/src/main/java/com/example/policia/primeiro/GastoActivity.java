383ed1f862ee0a2439a04ddb034e9a942e89dadb
Eduardo Mergulhao <ewtm@msn.com>
2017-10-10 17:52:25 -0300

    pag 87

--- app/src/main/java/com/example/policia/primeiro/GastoActivity.java
+++ app/src/main/java/com/example/policia/primeiro/GastoActivity.java
@@ -1,10 +1,70 @@
 package com.example.policia.primeiro;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 /**
  * Created by policia on 27/09/2017.
  */
 
-public class GastoActivity extends Activity {
+public class GastoActivity extends Activity  {
+
+    private int ano , mes , dia;
+    private Button dataGasto;
+    private Spinner categoria;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gasto);
+        Calendar calendar = Calendar.getInstance();
+        ano = calendar.get(Calendar.YEAR);
+        mes = calendar.get(Calendar.MONTH);
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dataGasto = (Button) findViewById(R.id.data);
+        dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,R.array.categoria_gasto,android.R.layout.simple_spinner_item);
+        categoria = (Spinner) findViewById(R.id.categoria);
+        categoria.setAdapter(adapter);
+        //preencher o spinner
+
+    }
+
+    public void selecionarData(View view){
+        showDialog(view.getId());
+    }
+
+    protected Dialog onCreateDialog(int id){
+        if(R.id.data == id){
+            return new DatePickerDialog(this,listener,ano,mes,dia);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            ano = year;
+            mes = month;
+            dia = dayOfMonth;
+            dataGasto.setText(dia + "/" + (mes+1) + "/" + ano);
+        }
+    };
+
+
 }
+
+
--- app/src/main/java/com/example/policia/primeiro/ViagemActivity.java
+++ app/src/main/java/com/example/policia/primeiro/ViagemActivity.java
@@ -3,15 +3,23 @@ package com.example.policia.primeiro;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
 
 /**
  * Created by policia on 27/09/2017.
  */
 
-public class ViagemActivity extends Activity {
+public class ViagemActivity extends Activity  implements
+        AdapterView.OnItemClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nova_viagem);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        
     }
 }
